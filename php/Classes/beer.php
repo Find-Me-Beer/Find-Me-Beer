0b13504209d457e4402c77ad27cabf708e316839
Reece Nunn <rmnunn5@gmail.com>
2020-02-07 10:06:13 -0700

    Wrote accessor and mutator for beer id

--- php/Classes/beer.php
+++ php/Classes/beer.php
@@ -58,7 +58,46 @@ class Beer implements \JsonSerializable {
 	 * @param String|Uuid $newBeerBreweryId
 	 * @param String $newBeerDescription description of this beer
 	 * @param String $newBeerName name of this beer
+	 * @param String $newBeerType type of this beer
 	 */
+	public function __construct($newBeerId, $newBeerAbv, $newBeerBreweryId, $newBeerDescription, $newBeerName, $newBeerType) {
+		try {
+			$this->setBeerId = $newBeerId;
+			$this->setBeerAbv = $newBeerAbv;
+			$this->setBeerBreweryId = $newBeerBreweryId;
+			$this->setBeerDescription = $newBeerDescription;
+			$this->setBeerName = $newBeerName;
+			$this->setBeerType = $newBeerType;
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw (new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
+	/**
+	 * accessor method for beer id
+	 * @return Uuid value of beer id
+	 */
+	public function getBeerId() {
+		return($this->beerId);
+	}
+	/**
+	 * mutator method for beer id
+	 *
+	 * @param String | Uuid $newBeerId new id of this beer
+	 * @throws \RangeException if $newBeerId is not positive
+	 * @throws \TypeError if $newBeerId is not an integer
+	 */
+	public function setBeerId($newBeerId) {
+		try {
+			$uuid = self::validateUuid($newBeerId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		$this->beerId = $uuid;
+	}
 
 
 }
