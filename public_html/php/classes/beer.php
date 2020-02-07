831f416f4b768f94762766858cdb302c0106f8b8
Reece Nunn <rmnunn5@gmail.com>
2020-02-06 21:40:06 -0700

    Finished constructor and getter / setter for beer id

--- public_html/php/classes/beer.php
+++ public_html/php/classes/beer.php
@@ -4,8 +4,6 @@ namespace ;
 require_once("autoload.php");
 require_once(dirname(__DIR__) . "/vendor/autoload.php");
 
-use Cassandra\Decimal;
-use MongoDB\BSON\Binary;
 use Ramsey\Uuid\Uuid;
 
 /**
@@ -55,8 +53,52 @@ class Beer implements \JsonSerializable {
 	 *
 	 * @param String|Uuid $newBeerId id of this beer
 	 * @param String|Decimal $newBeerAbv abv of this beer
-	 * @param String|Uuid
+	 * @param String|Uuid $newBeerBreweryId brewery id for this beer
+	 * @param String $newBeerDescription description of the beer
+	 * @param String $newBeerName name of the beer
+	 * @param String $newBeerType type of the beer
 	 */
+	public function __construct($newBeerId, $newBeerAbv, $newBeerBreweryId, $newBeerDescription, $newBeerName, $newBeerType) {
+		try {
+			$this->setBeerId($newBeerId);
+			$this->setBeerAbv($newBeerAbv);
+			$this->setBeerBreweryId($newBeerBreweryId);
+			$this->setBeerDescription($newBeerDescription);
+			$this->setBeerName($newBeerName);
+			$this->setBeerType($newBeerType);
+		}  //determine what exception type was thrown
+		catch(\InvalidArgumentException | \RangeException | \Exception | TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
 
+	/**
+	 * getter method for beer id
+	 *
+	 * @return Uuid value of beer id
+	 */
+	public function getBeerId(): void {
+		return ($this->beerId);
+	}
+
+	/**
+	 * setter method for beer id
+	 *
+	 * @param Uuid|String $newBeerId new value of beer id
+	 * @throws \RangeException if $newTweetId is not positive
+	 * @throws \TypeError if $newTweetId is not a uuid or string
+	 */
+	public function setBeerId($newBeerId): void {
+		$newBeerId = trim($newBeerId);
+		$newBeerId = filter_var($newBeerId, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		try {
+			$uuid = self::validateUuid($newBeerId);
+		} catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		$this->beerId = $uuid;
+	}
 
 }
