8a37f2b463afcaf1ddf0d0845a405b337f8bc67d
Reece Nunn <rmnunn5@gmail.com>
2020-02-12 15:31:01 -0700

    Created valid beer type, and brewery variables and creating dependent objects

--- php/Classes/Test/BeerTest.php
+++ php/Classes/Test/BeerTest.php
@@ -52,15 +52,35 @@ class BeerTest extends FindMeBeerTest {
 	 */
 	protected $VALID_BEERNAME = "Pine Nut Butterscotch Beer";
 
+	/**
+	 * valid beer type
+	 * @var $VALID_BEERTYPE
+	 */
+	protected $VALID_BEERTYPE = "Malt";
 
+	/**
+	 * brewery that created the beer; for foreign key relations
+	 * @var Brewery brewery
+	 */
+	protected $brewery = null;
 
-
-
+	/**
+	 * create dependent objects before running each test
+	 */
 
 	public final function setUp(): void {
 		//run setUp() method first
 		parent::setUp();
 
+		//create and insert brewery that created beer
+		$this->brewery = new Brewery(generateUuidV4(), "111 Marble Ave NW, Albuquerque, NM 87102",
+			"https://gravatar.com/avatar/07e75bbcdc08eca3d8db273bc7d3f7f8?s=400&d=robohash&r=x",
+			"Founded in 2008 in the heart of downtown Albuquerque, Marble Brewery is devoted to brewing
+			 premium craft beer that satisfies the thirsts and discriminating tastes of our diverse and loyal customers. 
+			 Not only do we brew quality craft beer classics, our fresh cutting-edge specials relentlessly push boundaries 
+			 and raise expectations. We package a variety of styles and distribute throughout New Mexico, Arizona, Southwest
+			 Texas and Southwest Colorado.", "marblebrewery@marble.com", "35.094880", "-106.665270",
+		"Marble Brewery", "(505)243-2739", "https://marblebrewery.com/");
 	}
 
 
