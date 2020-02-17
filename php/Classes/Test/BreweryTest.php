cad22ff2997fec31e40034835011ff167dd6b457
Celeste Whitaker <abqcc3@gmail.com>
2020-02-17 15:33:53 -0700

    Adding protected to BreweryTest.php

--- php/Classes/Test/BreweryTest.php
+++ php/Classes/Test/BreweryTest.php
@@ -43,20 +43,55 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 class BreweryTest extends FindMeBeerTest {
 	/**
-	 * Brewery activation token
-	 * @var string $VALID_ACTIVATION
+	 * @var int VALID_BREWERY_ID
 	 */
-	protected $VALID_ACTIVATION;
+	protected $VALID_BREWERY_ID = null;
 
-	/*
-	 *
-	 *
+	/**
+	 * address of brewery
+	 * @var string $VALID_BREWERY_ADDRESS
+	 */
+	protected $VALID_BREWERY_ADDRESS = "2316 56th st";
+
+	/**
+	 * @var string $VALID_BREWERY_AVATARURL
+	 */
+	protected $VALID_BREWERY_AVATARURL = "https://gravatar.com/avatar/60d845492a4bb0fa50049511ec139ce8?s=400&d=robohash&r=x";
+	/**
+	 * Description of Brewery
+	 * @var string $VALID_BREWERY_DESCRIPTION
+	 */
+	protected $VALID_BREWERY_DESCRIPTION = "Founded in 2008 in the heart of downtown Albuquerque, Marble Brewery is devoted to brewing premium craft beer that satisfies the thirsts and discriminating tastes of our diverse and loyal customers. Not only do we brew quality craft beer classics, our fresh cutting-edge specials relentlessly push boundaries and raise expectations.";
+	/**
+	 * @var string
 	 */
 
-	protected $
-}
+	protected $VALID_BREWERY_DESCRIPTION2 = "Brewery Description Test2";
+	/**
+	 * Email for brewery
+	 * @var string $VALID_BREWERY_EMAIL
+	 */
+	protected $VALID_BREWERY_EMAIL = "brewery@gmail.com";
+	/**
+	 * Testing Brewery Email
+	 * @var string $VALID_BREWERY_EMAIL2
+	 */
+	protected $VALID_BREWERY_EMAIL2 = "Brewery email test2";
 
+	/**
+	 * Brewery name
+	 * @var string $VALID_BREWERY_NAME
+	 */
+	protected $VALID_BREWERY_NAME ="Marble Brewery";
 
+	/**
+	 * Testing brewery name
+	 * @var string
+	 */
+	protected $VALID_BREWERY_NAME2 ="Testing brewery name";
 
-
-
+	/**
+	 * Brewery Latitude
+	 * @var string
+	 */
+	protected $VALID_BREWERY_LAT = "
