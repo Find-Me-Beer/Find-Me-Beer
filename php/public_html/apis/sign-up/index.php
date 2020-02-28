7a175c0007babce3fa94fbfff3ffe7e1ec498bca
Reece Nunn <rmnunn5@gmail.com>
2020-02-28 09:23:09 -0700

    Wrote prepare emoty reply, grabbed mysql connection and determine which HTTP method was used

--- php/public_html/apis/sign-up/index.php
+++ php/public_html/apis/sign-up/index.php
@@ -19,3 +19,23 @@ use FIndMeBeer\FindMeBeer\User;
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/beerme.ini");
+	$pdo = $secrets->getPdoObject();
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	if($method === "POST") {
+
+		//decode the json and turn it into a php object
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+	}
+}
