b4b852598eacfe9c056a284faab94cb31329277b
Celeste Whitaker <abqcc3@gmail.com>
2020-03-05 15:53:39 -0700

    Finished Tag API

--- php/public_html/apis/tag/index.php
+++ php/public_html/apis/tag/index.php
@@ -7,10 +7,10 @@ require_once dirname(__DIR__, 3) . "/lib/jwt.php";
 require_once dirname(__DIR__, 3) . "/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
-use \FindMeBeer\FindMeBeer\Favorite;
+use \FindMeBeer\FindMeBeer\Tag;
 
 /**
- * REST API for beer Tag
+ * REST API for Tag
  *
  * @author Celeste Whitaker <cwhitaker4@cnm.edu>
  */
@@ -18,7 +18,6 @@ use \FindMeBeer\FindMeBeer\Favorite;
 //verify the session, start if not active
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
-
 }
 
 //prepare an empty reply
@@ -27,7 +26,8 @@ $reply->status = 200;
 $reply->data = null;
 
 try {
-
+	//grab the mySQL connection
+	// grab mySQL connection
 	$secrets = new \Secrets("/etc/apache2/capstone-mysql/beerme.ini");
 	$pdo = $secrets->getPdoObject();
 
@@ -35,4 +35,26 @@ try {
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
 	//sanitize input
-	$id =
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$tagContent = filter_input(INPUT_GET, "tagContent", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+	if ($method === "GET"){
+		//set XSRF cookie
+		setXsrfCookie();
+
+		if(empty($id) === false) {
+			$reply->data = Tag::getTagByTagId($pdo, $id);
+		} else {
+			$reply->data = Tag::getAllTags($pdo)->toArray();
+		}
+	} else {
+		throw (new InvalidArgumentException("Invalid HTTP method request"));
+	}
+} catch(\Exception | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+}
+
+// encode and return reply to front end caller
+header("Content-type: application/json");
+echo json_encode($reply);
