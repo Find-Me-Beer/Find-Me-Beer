349242063e320e9a20b7e5c730be1baf8fda480c
Vladimir Arias-Antonov <ariasantonov93@gmail.com>
2020-03-09 16:54:01 -0700

    sprint-04 react spun up

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter} from "react-router-dom";
+import {Route, Switch} from "react-router";
+import {FourOhFour} from "./pages/FourOhFour";
+import {Home} from "./pages/Home";
 
-const App = () => ( <h1 className="text-info">Hello Reece</h1> );
-ReactDOM.render(<App/>, document.querySelector('#root'));
+const Routing = () => (
+	<>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home}/>
+				<Route component={FourOhFour}/>
+			</Switch>
+		</BrowserRouter>
+	</>
+);
+ReactDOM.render(<Routing/>, document.querySelector('#root'));
+
+
+// use https://github.com/rlewis2892/creepy-octo-meow-react/blob/react-hooks/app/src/index.js as reference
