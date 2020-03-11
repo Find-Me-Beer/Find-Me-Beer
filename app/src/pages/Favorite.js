a351e977c13cdbb21d3e4241744aeba874fd62f6
Reece Nunn <rmnunn5@gmail.com>
2020-03-11 13:21:55 -0600

    Added Favorite Page and code

--- app/src/pages/Favorite.js
+++ app/src/pages/Favorite.js
@@ -5,9 +5,10 @@ import {UseJwt} from "../shared/misc/JwtHelpers";
 import {handleSessionTimeout} from "../shared/misc/handle-session-timeout";
 import _ from "lodash";
 
-import Badge from "react-bootstrap/Badge";
 import Button from "react-bootstrap/Button";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+//Based on Example from Rochelle Lewis
 
 export const Favorite = ({favoriteBeerId, favoriteUserId}) => {
 
@@ -15,37 +16,92 @@ export const Favorite = ({favoriteBeerId, favoriteUserId}) => {
 	const jwt = UseJwt();
 	const [isFavorited, setIsFavorited] = useState(null);
 
-	// Grab all likes from the redux store
-	const likes = useSelector(state => (state.favorites ? state.favorite : []));
+	// Grab all favorites from the redux store
+	const favorites = useSelector(state => (state.favorites ? state.favorites : []));
 
 	const effects = () => {
 		initializeFavorites(userId);
 	};
 
-	// add likes to inputs - this informs React that likes are being updated from Redux. This ensures proper component rendering.
+	// add favorites to inputs - this informs React that favorites are being updated from Redux. This ensures proper component rendering.
 	const inputs = [favorites, userId, beerId];
 	useEffect(effects, inputs);
 
 	const initializeFavorites = (userId) => {
 		const userFavorites = favorites.filter(favorite => favorite.favoriteUserId === userId);
-		const favorited = _.find(userLikes, {'favoriteBeerId': beerId});
-		return (_.isEmpty(favorited) === false) && setIsLiked("active");
+		const favorited = _.find(userFavorites, {'favoriteBeerId': beerId});
+		return (_.isEmpty(favorited) === false) && setIsFavorited("active");
 	};
 
+	/*
+	* data object gets passed in Axios POST and DELETE requests.
+	* See submitFavorite and deleteFavorite below.
+	* */
 	const data = {
 		favoriteBeerId: beerId,
 		favoriteUserId: userId
 	};
 
+	/*
+	* toggleFavorite gets called when a Favorite is successfully created or deleted by the user.
+	* This toggles the state of the isFavorited variable.
+	*
+	* See submitFavorite and deleteFavorite.
+	* */
+	const toggleFavorite = () => {
+		setIsFavorited(isFavorited === null ? "active" : null);
+	};
 
+	/*
+	* User posts a Favorite.
+	* */
+	const submitFavorite = () => {
+		const headers = {'X-JWT-TOKEN': jwt};
+		httpConfig.post("/apis/favorite/", data, {
+			headers: headers})
+			.then(reply => {
+				let {message, type} = reply;
+				if(reply.status === 200) {
+					toggleFavorite();
+				}
+				// if there's an issue with a $_SESSION mismatch with xsrf or jwt, alert user and do a sign out
+				if(reply.status === 401) {
+					handleSessionTimeout();
+				}
+			});
+	};
 
+	/*
+* User deletes a Favorite.
+* */
+	const deleteFavorite = () => {
+		const headers = {'X-JWT-TOKEN': jwt};
+		httpConfig.delete("/apis/favorite/", {
+			headers, data})
+			.then(reply => {
+				let {message, type} = reply;
+				if(reply.status === 200) {
+					toggleFavorite();
+				}
+				// if there's an issue with a $_SESSION mismatch with xsrf or jwt, alert user and do a sign out
+				if(reply.status === 401) {
+					handleSessionTimeout();
+				}
+			});
+	};
 
+	/*
+	* Fire this function onclick
+	* */
+	const clickFavorite = () => {
+		(isFavorited === "active") ? deleteFavorite() : submitFavorite();
+	};
 
-
-
-
-
-
-
-
-}
+	return (
+		<>
+			<Button variant="outline-danger" size="sm" className={`post-favorite-btn ${(isFavorited !== null ? isFavorited : "")}`} disabled={!jwt && true} onClick={clickFavorite}>
+				<FontAwesomeIcon icon="star"/>&nbsp;
+			</Button>
+		</>
+	)
+};
