7e061806cfcd342d59ba9f539be973aa97192d5f
patrickleyba <pleyba4@cnm.edu>
2020-03-10 23:06:15 -0600

    savepoint making home page

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,9 +1,23 @@
 import React from "react"
+import {Link} from "react-router-dom";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from 'image/fmb-beer.jpg';
 
 export const Home = () => {
 	return (
 		<>
-			<h1>Home</h1>
+			<main>
+				<Container>
+					<Row>
+						<Col xs={6} md={4}>
+							<Image src="/image/fmb-beer.jpg" fluid />
+						</Col>
+					</Row>
+				</Container>
+			</main>
 		</>
-	)
+	);
 }
