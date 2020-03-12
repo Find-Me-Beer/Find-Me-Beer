9c6a6f5d5f881065d46f5c1b1d1c4592eaba60be
Reece Nunn <rmnunn5@gmail.com>
2020-03-12 16:54:40 -0600

    added more actions to brewery and favorite

--- app/src/shared/actions/get-breweries.js
+++ app/src/shared/actions/get-breweries.js
@@ -6,3 +6,20 @@ export const getAllBreweries = () => async dispatch => {
 	const {data} = await httpConfig(`/apis/brewery/`);
 	dispatch({type: "GET_ALL_BREWERIES", payload: data})
 };
+
+export const getBreweryByBreweryId = (breweryId) => async dispatch => {
+	const {data} = await httpConfig(`/apis/brewery/?breweryId=${breweryId}`);
+	dispatch({type: "GET_BREWERY_BY_BREWERY_ID", payload: data})
+};
+
+export const getBreweryByLocation = (userLat, userLong, distance) => async dispatch => {
+	const {data} = await httpConfig(`/apis/brewery/?breweryLocation=${userLat}&${userLong}&${distance}`);
+	dispatch({type: "GET_BREWERY_BY_LOCATION", payload: data})
+};
+
+export const getBreweryByBreweryName = (breweryName) => async dispatch => {
+	const {data} = await httpConfig(`/apis/brewery/?breweryName=${breweryName}`);
+	dispatch({type: "GET_BREWERY_BY_BREWERY_NAME", payload: data})
+};
+
+
