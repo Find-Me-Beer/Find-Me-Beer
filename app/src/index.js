fe3d0a65204c951b38ede5daddde48e00cb8fb44
patrickleyba <pleyba4@cnm.edu>
2020-03-12 14:20:46 -0600

    savepoint

--- app/src/index.js
+++ app/src/index.js
@@ -3,21 +3,27 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
-import {FourOhFour} from "./pages/FourOhFour";
-import {Home} from "./pages/Home";
 
 import 'bootstrap/dist/css/bootstrap.css';
 
+import "./index.css";
+import {Home} from "./pages/Home";
+import {FourOhFour} from "./pages/FourOhFour";
+import {applyMiddleware, createStore} from "redux";
+import thunk from "redux-thunk";
 
+const store = createStore(applyMiddleware(thunk));
 
 const Routing = () => (
 	<>
+		<Provider store={store}>
 		<BrowserRouter>
 			<Switch>
 				<Route exact path="./pages/home" component={Home}/>
 				<Route component={FourOhFour}/>
 			</Switch>
 		</BrowserRouter>
+		</Provider>
 	</>
 );
 ReactDOM.render(<Routing/>, document.querySelector('#root'));
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,21 +1,48 @@
-import React from "react"
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Image from 'shared/image/fmb-beer.jpg';
+import {Button} from "react-bootstrap";
+
+/**
+ * @return {undefined}
+ */
+function UseJwt() {
+
+	return undefined;
+}
 
 export const Home = () => {
+
+	// grab the jwt to check for logged in users. See JwtHelpers.js
+	const jwt = UseJwt();
+
 	return (
 		<>
-			<main>
-				<Container>
+			<main className="d-flex align-items-center mh-100">
+				<Container fluid="true">
 					<Row>
-						<Col xs={6} md={4}>
-							<Image src="/image/fmb-beer.jpg" alt="beer" fluid />
+						<Col sm={6} lg={{span: 4, offset: 1}}>
+
+							{/* only render the signin form if user does not have a jwt, otherwise output a message */}
+							{jwt === null ? (
+								<SignInForm/>
+							) : (
+								<div>
+									<span className="h2 mr-2">You're logged in!</span>
+									&nbsp;
+									<Link to="/posts">
+										<Button className="btn-sm mb-2" variant="outline-dark">Head to Posts&nbsp;&nbsp;
+										</Button>
+									</Link>
+								</div>
+							)}
+
 						</Col>
 					</Row>
 				</Container>
 			</main>
+
 		</>
-	);
+	)
 };
