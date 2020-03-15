a9ed36d57bc21dec3b8a6351e4783adc6669d162
Vladimir Arias-Antonov <ariasantonov93@gmail.com>
2020-03-15 13:05:04 -0700

    get userjs action added

--- app/src/shared/actions/getUser.js
+++ app/src/shared/actions/getUser.js
@@ -1,7 +1,7 @@
 import {httpConfig} from "../misc/http-config";
 
-export const getUserByUserId = () => async dispatch => {
-	const {data} = await httpConfig('apis/user/');
+export const getUserByUserID = (id) => async dispatch => {
+	const {data} = await httpConfig('apis/user/${id}');
 	dispatch({type: "GET_USER_BY_USER_ID", payload: data })
 };
 
