8f0a300ccaf73963413ef52b97b179a8fd0173ed
patrickleyba <pleyba4@cnm.edu>
2020-03-15 21:47:17 -0600

    Working on layout added button and need to confirm components for homepage

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -2,13 +2,13 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 import Image from "react-bootstrap/Image";
 import Image1 from "./shared/image/fmb-beer.jpg";
 import Image2 from "./shared/image/beer-mug.png";
 import Image3 from "./shared/image/location-tick.png";
 import Image4 from "./shared/image/favorite-star.png";
-
 
 
 export const Home = () => {
@@ -19,23 +19,34 @@ export const Home = () => {
 				<Container fluid="true">
 					<Row>
 						<Col className="col-12">
-							<Image className="img-fluid" src={Image1} alt="beer foam in a glass" id="background-image" fluid />
+							<Image className="img-fluid" src={Image1} alt="beer foam in a glass" id="background-image" fluid/>
 						</Col>
 					</Row>
 				</Container>
 				<Container fluid="true">
 					<Row>
-						<Col className="col-6 offset-3 col-sm-4 offset-sm-0">
-							<Image src={Image2} className="thumbs img-fluid"/>
+						<Col className="col-6 offset-3 col-sm-4 offset-sm-0 mt-5 mb-3">
+							<Image src={Image2} className="thumbs img-thumbnail"/>
 							<div>Discover New Beer Based On Preferences.</div>
 						</Col>
-						<Col className="col-6 offset-3 col-sm-4 offset-sm-0">
+						<Col className="col-6 offset-3 col-sm-4 offset-sm-0 mt-5 mb-3">
 							<Image src={Image3} className="thumbs img-fluid"/>
 							<div>Find Local Breweries Near Your Location.</div>
 						</Col>
-						<Col className="col-6 offset-3 col-sm-4 offset-sm-0">
+						<Col className="col-6 offset-3 col-sm-4 offset-sm-0 mt-5 mb-5">
 							<Image src={Image4} className="thumbs img-fluid" class="thumbs"/>
 							<div>Discover New Beer Based On Preferences.</div>
+						</Col>
+					</Row>
+				</Container>
+				<Container fluid="true">
+					<Row>
+						<Col className="col-12 offset-4">
+							<div className="mb-5">
+								<Button variant="primary" size="lg">
+									Find Me Beer
+								</Button>{' '}
+							</div>
 						</Col>
 					</Row>
 				</Container>
