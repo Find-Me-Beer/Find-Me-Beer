be6fadb3912e434d19e65d1c6570e34486b103db
Reece Nunn <rmnunn5@gmail.com>
2020-03-17 14:59:29 -0600

    Updated User Reducer

--- app/src/shared/reducers/UserReducer.js
+++ app/src/shared/reducers/UserReducer.js
@@ -3,7 +3,7 @@ export function userReducer (state = [], action) {
 		case "GET_USER_BY_USER_ID":
 			return [...state, action.payload];
 		case "GET_USER_BY_USER_EMAIL":
-			return action.payload;
+			return [...state, action.payload];
 		default:
 			return state;
 	}
