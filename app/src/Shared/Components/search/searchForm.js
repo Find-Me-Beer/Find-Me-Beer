c0edfb9cfee2c8c563495d8e92af906a879d7124
Reece Nunn <rmnunn5@gmail.com>
2020-03-17 09:18:16 -0600

    added search form component

---
+++ app/src/Shared/Components/search/searchForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+​
+// import FormControl from "react-bootstrap/FormControl";
+​
+export const SearchFormContent = ({setSearchTerm}) => {
+	const setSearch = (e) => {
+		e.preventDefault();
+		//check the input field for which characters are being entered and set them as the search term
+		setSearchTerm(e.target.value);
+	};
+​
+	return (
+		<>
+			<Form inline className="justify-content-center p-5">
+				<Form.Control type="text"
+								  placeholder="Search for recipe... "
+								  id="search-text"
+								  onChange={setSearch}
+								  onSubmit={setSearch}
+				/>
+			</Form>
+		</>
+	);
+};
