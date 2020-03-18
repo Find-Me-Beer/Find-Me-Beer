358d3475ead1cfee36067cef7559e9f2fd66b882
Rochelle Lewis <rlewis2892@users.noreply.github.com>
2020-03-18 04:31:10 -0600

    Favorite API cleanup
    structural cleanup. try this and see if it helps with favorite functionality. Needs testing.

--- php/public_html/apis/favorite/index.php
+++ php/public_html/apis/favorite/index.php
@@ -2,13 +2,12 @@
 
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
-require_once("/etc/apache2/capstone-mysql/Secrets.php");
 require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/lib/jwt.php";
 require_once dirname(__DIR__, 3) . "/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
-use FindMeBeer\FindMeBeer\{Favorite, Brewery, Beer};
+use FindMeBeer\FindMeBeer\{Favorite};
 
 /**
  * Api for the Favorite class
@@ -34,7 +33,6 @@ try {
 	//determine which HTTP method was used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
-
 	//sanitize the search parameters
 	$favoriteBeerId = $id = filter_input(INPUT_GET, "favoriteBeerId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
 	$favoriteUserId = $id = filter_input(INPUT_GET, "favoriteUserId", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
@@ -43,18 +41,15 @@ try {
 		//set XSRF cookie
 		setXsrfCookie();
 
-		//gets  a specific like associated based on its composite key
-		if ($favoriteBeerId !== null && $favoriteUserId !== null) {
+		//gets a specific like associated based on its composite key
+		if (empty($favoriteBeerId) === false && empty($favoriteUserId) === false) {
 			$favorite = Favorite::getFavoriteByFavoriteBeerIdAndFavoriteUserId($pdo, $favoriteBeerId, $favoriteUserId);
 
-
+			/*whut?
 			if($favorite!== null) {
 				$reply->data = $favorite;
-			}
-			//if none of the search parameters are met throw an exception
-		//else if(empty($favoriteBeerId) === false) {
-			//$reply->data = Favorite::getFavoriteByFavoriteBeerId($pdo, $favoriteBeerId)->toArray();
-			//get all the likes associated with the beerId
+			}*/
+
 		} else if(empty($favoriteUserId) === false) {
 			$reply->data = Favorite::getFavoriteByFavoriteUserId($pdo, $favoriteUserId)->toArray();
 		} else {
@@ -62,6 +57,15 @@ try {
 		}
 
 	} else if($method === "POST" || $method === "PUT") {
+
+		//enforce that the end user has an XSRF token.
+		verifyXsrf();
+		validateJwtHeader();
+
+		// enforce the user is signed in
+		if(empty($_SESSION["user"]) === true) {
+			throw(new \InvalidArgumentException("you must be logged in to favorite beer", 403));
+		}
 
 		//decode the response from the front end
 		$requestContent = file_get_contents("php://input");
@@ -75,48 +79,30 @@ try {
 			throw (new \InvalidArgumentException("No User linked to the Favorite", 405));
 		}
 
-
 		if($method === "POST") {
 
-			//enforce that the end user has an XSRF token.
-			verifyXsrf();
-
-			//enforce the end user has a JWT token
-			//validateJwtHeader();
-
-			// enforce the user is signed in
-			if(empty($_SESSION["user"]) === true) {
-				throw(new \InvalidArgumentException("you must be logged in to favorite beer", 403));
+			//check if the favorite already exists
+			$favoriteCheck = Favorite::getFavoriteByFavoriteBeerIdAndFavoriteUserId($pdo, $requestObject->favoriteBeerId, $_SESSION["user"]->getUserId());
+			if(!empty($favoriteCheck) || $favoriteCheck !== null) {
+				throw (new \InvalidArgumentException("You've already liked this beer.", 403));
 			}
 
-			validateJwtHeader();
-			var_dump($requestObject->favoriteBeerId);
-			var_dump($_SESSION);
 			$favorite = new Favorite($requestObject->favoriteBeerId, $_SESSION["user"]->getUserId());
 			$favorite->insert($pdo);
 			$reply->message = "favorite beer successful";
 
-
 		} else if($method === "PUT") {
-
-			//enforce the end user has a XSRF token.
-			verifyXsrf();
-
-			//enforce the end user has a JWT token
-			validateJwtHeader();
 
 			//grab the like by its composite key
 			$favorite = Favorite::getFavoriteByFavoriteBeerIdAndFavoriteUserId($pdo, $requestObject->favoriteBeerId, $requestObject->favoriteUserId);
 			if($favorite === null) {
-				throw (new RuntimeException("Like does not exist"));
+				throw (new RuntimeException("Favorite does not exist"));
 			}
 
-			//enforce the user is signed in and only trying to edit their own like
+			//enforce the user is signed in and only trying to delete their own favorite
 			if(empty($_SESSION["user"]) === true || $_SESSION["user"]->getUserId()->toString() !== $favorite->getFavoriteUserId()->toString()) {
-				throw(new \InvalidArgumentException("You are not allowed to delete this beer", 403));
+				throw(new \InvalidArgumentException("You are not allowed to unfavorite this beer.", 403));
 			}
-
-			//validateJwtHeader();
 
 			//preform the actual delete
 			$favorite->delete($pdo);
@@ -127,8 +113,9 @@ try {
 
 		// if any other HTTP request is sent throw an exception
 	} else {
-		throw new \InvalidArgumentException("invalid http request", 400);
+		throw new \InvalidArgumentException("invalid http request", 405);
 	}
+
 	//catch any exceptions that is thrown and update the reply status and message
 } catch(\Exception | \TypeError $exception) {
 	$reply->status = $exception->getCode();
