168cf2700463fc01d604421da7b695247782f758
patrickleyba <pleyba4@cnm.edu>
2020-03-18 17:33:23 -0600

    savepoint styling sign in modal and fixed testing code that was displaying for testing purposes

--- app/src/shared/components/sign-in/SigninModal.js
+++ app/src/shared/components/sign-in/SigninModal.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
-import {Button} from "react-bootstrap";
+import {Button, Col, Image, Row} from "react-bootstrap";
 import {Modal} from "react-bootstrap";
 import {SignInForm} from "./SignInForm";
+import Logo from "../../../img/fmb-navbar-logo-gray.png"
 
 
 export const SignInModal = () => {
@@ -16,10 +17,13 @@ export const SignInModal = () => {
 				Sign In!
 			</Button>
 
-			<Modal show={show} onHide={handleClose}>
+			<Modal show={show} onHide={handleClose} className="modal-dark">
 				<Modal.Header closeButton>
-					<Modal.Title>Sign In</Modal.Title>
+					<Modal.Title className="modal-title">FMB Sign In!</Modal.Title>
 				</Modal.Header>
+				<Row className="col-12">
+				<Image src={Logo} className="logo-icon-gray offset-3 mt-5" />
+				</Row>
 				<Modal.Body>
 					<SignInForm/>
 				</Modal.Body>
