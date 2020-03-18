30e59cdfaf3417fd33ebf7af8798a26c11831d89
patrickleyba <pleyba4@cnm.edu>
2020-03-17 20:21:45 -0600

    added background image and logo to home page

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -5,11 +5,10 @@ import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
 import Image from "react-bootstrap/Image";
-import Image1 from "../img/fmb-beer.jpg";
 import Image2 from "../img/beer-mug.png";
 import Image3 from "../img/location-tick.png";
 import Image4 from "../img/favorite-star.png";
-
+import Logo from "../img/fmb-navbar-logo.png"
 
 
 export const Home = () => {
@@ -17,25 +16,23 @@ export const Home = () => {
 	return (
 		<>
 			<main className="mh-100">
-				<div className="home-background">
-				<Container fluid="true">
-					<Row>
-<<<<<<< HEAD
-						<Col className="col-12">
-							<h1 className="home-page-text">Welcome To The Find Me Beer App</h1>
-							{/*<Image className="img-fluid" src={Image1} alt="beer foam in a glass" id="background-image" fluid/>*/}
-						</Col>
-					</Row>
-				</Container>
+				<div className="home-background mh-100">
+					<Container fluid="true">
+						<Row>
+							<Col className="col-12">
+								<h1 className="home-page-text-yellow mt-5 mb-5">Welcome!</h1>
+									<Col className="col-12 offset-5">
+										<Image className="logo-icon-large" src={Logo} />
+										<p>Find Me Beer has you covered when it comes to finding the perfect craft beer and then it will take you to it! </p>
+								</Col>
+							</Col>
+						</Row>
+					</Container>
 				</div>
 				<Container>
 					<Row>
 						<Col>
-							<h1 className="mt-5  col-9">FMB Features</h1>
-=======
-						<Col className="col-12 pr-0">
-							<Image className="img-fluid" src={Image1} alt="beer foam in a glass" id="background-image" fluid/>
->>>>>>> 4e07123a453a18882026fe280f83603f142b00ee
+							<h3 className="display-4 mt-5 d-bloc home-page-text-black">FMB! Features</h3>
 						</Col>
 					</Row>
 				</Container>
@@ -46,11 +43,11 @@ export const Home = () => {
 							<h3 className="ml-3 align-text-center home-page-text">Discover New Beer Based On Preferences.</h3>
 						</Col>
 						<Col m={4}>
-							<Image src={Image3} className="thumbs thumbs mx-auto d-block"/>
+							<Image src={Image3} className="thumbs mx-auto d-block"/>
 							<h3 className="ml-1 align-text-center home-page-text">Find Local Breweries Near Your Location.</h3>
 						</Col>
 						<Col m={4}>
-							<Image src={Image4} className="thumbs thumbs mx-auto d-block pr-0"/>
+							<Image src={Image4} className="thumbs mx-auto d-block"/>
 							<h3 className="align-text-center home-page-text">Keep a list of your favorite beer.</h3>
 						</Col>
 					</Row>
@@ -58,9 +55,9 @@ export const Home = () => {
 				<Container fluid="true" className="pb-5">
 					<Row>
 						<Col m={12}>
-								<Button variant="primary" size="lg" className="rounded mx-auto d-block">
-									Find Me Beer!
-								</Button>{' '}
+							<Button variant="primary" size="lg" className="home-page-text rounded mx-auto d-block">
+								Find Me Beer!
+							</Button>{' '}
 						</Col>
 					</Row>
 				</Container>
