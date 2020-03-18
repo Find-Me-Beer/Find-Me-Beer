79c519fb145499fe656e036fd23d39249c82fbf8
Reece Nunn <rmnunn5@gmail.com>
2020-03-18 08:58:12 -0600

    Updated favorite-fix with Rochelle's fix.

--- app/src/setupProxy.js
+++ app/src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
 	app.use(proxy('/apis', {
 		logLevel: 'debug',
-		target: "https://bootcamp-coders.cnm.edu/~variasantonov/Find-Me-Beer/php/public_html/",
+		target: "https://bootcamp-coders.cnm.edu/~rnunn4/Find-Me-Beer/php/public_html/",
 		changeOrigin: true,
 		secure: true, }));
 };
