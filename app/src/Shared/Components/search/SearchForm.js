e10ed927cbabb7049e8bf764d59995286b983419
Reece Nunn <rmnunn5@gmail.com>
2020-03-19 10:30:12 -0600

    Working on a Search Bar

--- app/src/Shared/Components/search/SearchForm.js
+++ app/src/Shared/Components/search/SearchForm.js
@@ -1,20 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {useSelector} from "react-redux";
 
 export const Search = () => {
+
+	const beer = useSelector((state) => state.beer ? state.beer : null);
+
 	const [searchTerm, setSearchTerm] = React.useState("");
 	const [searchResults, setSearchResults] = React.useState([]);
 	const handleChange = e => {
 		setSearchTerm(e.target.value);
 	};
 	React.useEffect(() => {
-		const results = people.filter(person =>
-			person.toLowerCase().includes(searchTerm)
+		const results = beer.filter(beer =>
+			beer.toLowerCase().includes(searchTerm)
 		);
 		setSearchResults(results);
 	}, [searchTerm]);
 	return (
-		<div className="App">
+		<div className="Search">
 			<input
 				type="text"
 				placeholder="Search"
@@ -22,10 +26,8 @@ export const Search = () => {
 				onChange={handleChange}
 			/>
 			<ul>
-				{searchResults.map(item => (
-					<li>{item}</li>
-				))}
+				{searchResults.map(item => (<li>{item}</li>))}
 			</ul>
 		</div>
 	);
-}
+};
