f164e5506761de17277a7f9d4cad795d3f9859a2
patrickleyba <pleyba4@cnm.edu>
2020-03-18 18:09:48 -0600

    savepoint styling modal with bg color

--- app/src/shared/components/sign-in/SigninModal.js
+++ app/src/shared/components/sign-in/SigninModal.js
@@ -17,14 +17,14 @@ export const SignInModal = () => {
 				Sign In!
 			</Button>
 
-			<Modal show={show} onHide={handleClose} className="modal-dark">
-				<Modal.Header closeButton>
-					<Modal.Title className="modal-title">FMB Sign In!</Modal.Title>
+			<Modal show={show} onHide={handleClose} className="modal-bg-yellow">
+				<Modal.Header closeButton className="modal-bg-yellow">
+					<Modal.Title className="modal-bg-yellow">FMB Sign In!</Modal.Title>
 				</Modal.Header>
-				<Row className="col-12">
-				<Image src={Logo} className="logo-icon-gray offset-3 mt-5" />
+				<Row className="col-12 modal-bg-yellow">
+				<Image src={Logo} className="modal-bg-yellow logo-icon-gray offset-3 mt-5" />
 				</Row>
-				<Modal.Body>
+				<Modal.Body className="modal-bg-yellow">
 					<SignInForm/>
 				</Modal.Body>
 			</Modal>
