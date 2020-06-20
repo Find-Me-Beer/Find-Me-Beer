6c657572a9e1448525904f51c98f950a1fd681b8
Reece Nunn <rmnunn5@gmail.com>
2020-06-19 20:18:31 -0600

    Added DOcker and deployment files from Vlad Mar. 21 Sprint 5 branch commit

---
+++ app/src/setupProxy.js
@@ -0,0 +1,8 @@
+const proxy = require('http-proxy-middleware');
+module.exports = function(app) {
+	app.use(proxy('/apis', {
+		logLevel: 'debug',
+		target: "https://bootcamp-coders.cnm.edu/~variasantonov/Find-Me-Beer/php/public_html/",
+		changeOrigin: true,
+		secure: true, }));
+};
